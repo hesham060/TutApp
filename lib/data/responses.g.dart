2d53ccea8f714a24e77082554a346bc7fac2d69b
hesham060 <heshamkhaled060@gmail.com>
2022-09-18 20:54:30 +0300

    45

--- lib/data/responses.g.dart
+++ lib/data/responses.g.dart
@@ -43,3 +43,25 @@ Map<String, dynamic> _$ContactsResponseToJson(ContactsResponse instance) =>
       'email': instance.email,
       'link': instance.link,
     };
+
+AuthenticationResponse _$AuthenticationResponseFromJson(
+        Map<String, dynamic> json) =>
+    AuthenticationResponse(
+      json['customer'] == null
+          ? null
+          : CustomerResponse.fromJson(json['customer'] as Map<String, dynamic>),
+      json['contacts'] == null
+          ? null
+          : ContactsResponse.fromJson(json['contacts'] as Map<String, dynamic>),
+    )
+      ..status = json['status'] as int?
+      ..message = json['message'] as String?;
+
+Map<String, dynamic> _$AuthenticationResponseToJson(
+        AuthenticationResponse instance) =>
+    <String, dynamic>{
+      'status': instance.status,
+      'message': instance.message,
+      'customer': instance.customer,
+      'contacts': instance.contacts,
+    };
