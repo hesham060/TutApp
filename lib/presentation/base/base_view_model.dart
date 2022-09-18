13817c024e8948743b6894b360dcef0956d9ff59
hesham060 <heshamkhaled060@gmail.com>
2022-09-18 08:54:50 +0300

    first lifting

---
+++ lib/presentation/base/base_view_model.dart
@@ -0,0 +1,13 @@
+abstract class BaseViewModel extends BaseViewModelInputs with BaseViewModelOuts{
+
+}
+abstract class BaseViewModelInputs{
+void start();         // start view model job
+void dispose();       // will be called when view model dies 
+
+}
+abstract class BaseViewModelOuts{
+
+  
+}
+  
