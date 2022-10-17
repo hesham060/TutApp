dfffb99e568738e126aa0d7d6579efc774f48972
hesham060 <heshamkhaled060@gmail.com>
2022-10-17 14:13:21 +0300

    dependency injection

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
-import 'package:firstproject/tests/rating_bar.dart';
+import 'package:firstproject/app/di.dart';
 import 'package:flutter/material.dart';
 
 import 'app/app.dart';
 
-void main() {
-  runApp( MyApp());
+void main() async {
+  // we use one for make sure any future instace ready 
+  WidgetsFlutterBinding.ensureInitialized();
+  // this one hold all instance we should put before start program
+  await initAppModule();
+
+  runApp(MyApp());
 }
-
-
